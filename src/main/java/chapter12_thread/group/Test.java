e48fc0ab1c0bf8b622ccd3f2405b106d5a7a8964
kmss6905 <kmss69052@naver.com>
2022-10-18 00:26:22 +0900

    12 - 결과물을 리턴 받는 스레드 풀 작업

--- src/main/java/chapter12_thread/group/Test.java
+++ src/main/java/chapter12_thread/group/Test.java
@@ -3,8 +3,36 @@ package chapter12_thread.group;
 import java.util.concurrent.*;
 
 public class Test {
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // create thread pool
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 100; i++) {
+            Callable<String> callable = new Callable<>() {
+                @Override
+                public String call() throws Exception {
+                    ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executorService;
+                    int poolSize = threadPoolExecutor.getPoolSize();
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println("[총 스레드 개수: " + poolSize + "] 작업 스레드 이름: " + threadName);
+                    return threadName;
+                }
+            };
+
+            // task 요청
+            Future<String> submit = executorService.submit(() -> {
+                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executorService;
+                int poolSize = threadPoolExecutor.getPoolSize();
+                String threadName = Thread.currentThread().getName();
+                System.out.println("[총 스레드 개수: " + poolSize + "] 작업 스레드 이름: " + threadName);
+                return threadName;
+            });
+
+            // block
+            String s = submit.get();
+            System.out.println("name : " + s);
+        }
+
 
     }
 }
